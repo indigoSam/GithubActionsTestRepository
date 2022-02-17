ce86d92ae1af1e1d9316c39318ee65783da8f31b
Sam Silver <ssilver@indigoag.com>
2022-02-17 15:34:55 -0600

    Add some changes to the SDK in indigoag/ads

--- indigoag/ads/utils/my_cool_util.py
+++ indigoag/ads/utils/my_cool_util.py
@@ -1,2 +1,4 @@
 # This is a standin for some file nested within the indigoag/ads 
 # subdirectory. Any change to a file the indigoag.ads subdir should trigger the workflow.
+
+print("Here's a change to the SDK!")
