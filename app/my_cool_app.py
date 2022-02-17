63b16acf83971649afed0f957db12823e7eaa954
Sam Silver <ssilver@indigoag.com>
2022-02-17 16:35:38 -0600

    Change to main App

--- app/my_cool_app.py
+++ app/my_cool_app.py
@@ -4,6 +4,7 @@ def do_the_app():
     print("I've made some changes to my app!")
     print("Wow, here are some more changse!")
     print("Here are some more changes")
+    print("Change to main app")
 
 if __name__=="__main__":
     do_the_app()
