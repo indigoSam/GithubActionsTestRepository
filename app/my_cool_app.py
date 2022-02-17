e02f6c17f2e5502c76811d33037e0f5473d2baf8
Sam Silver <ssilver@indigoag.com>
2022-02-17 15:40:25 -0600

    Good work, Sam

--- app/my_cool_app.py
+++ app/my_cool_app.py
@@ -3,6 +3,7 @@ def do_the_app():
     print("The app is being done.")
     print("I've made some changes to my app!")
     print("Wow, here are some more changse!")
+    print("Cool, another feature.")
 
 if __name__=="__main__":
     do_the_app()
